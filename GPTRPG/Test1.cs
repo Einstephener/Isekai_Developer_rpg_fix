4326f49eb913209942c3da47cc6b4deb11856922
조성우 <chomuscleguy@gmail.com>
2023-11-16 11:40:34 +0900

    commit

---
+++ GPTRPG/Test1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPTRPG
+{
+    internal class Test1
+    {
+        //테스트 해보겠읍니다.
+    }
+}
